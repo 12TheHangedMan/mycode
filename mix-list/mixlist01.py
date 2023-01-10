2ab71f03c07b2e412a353b313f8abbdb7105c5b2
12TheHangedMan <chongwei.ma@outlook.com>
2023-01-10 17:34:40 +0000

    mixed lists2

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -12,3 +12,18 @@ print("The second item in the list (port): " + str(my_list[1]))
 # display the third item in the list
 print("The last item in the list (state): " + my_list[2])
 
+
+
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+# example 2 - use the comma separator
+print("IP addresses:", iplist[3].strip(), ", and", iplist[4])
+
+# example 3 - use an 'f-string'
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
+
