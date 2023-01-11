aad7cb07bf837703ed9f8ecb8b2c6880a9ba8784
12TheHangedMan <chongwei.ma@outlook.com>
2023-01-11 23:03:30 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,8 @@
+print("This code will always execute.")
+
+def main():
+   print("I modified This code belongs to the main function in module 1.")
+
+if __name__ == "first_module.main()":
+   main()
+
