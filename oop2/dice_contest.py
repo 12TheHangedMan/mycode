c094eaa95cf5bf2aa3bf9cc6d156cf95cae1685c
12TheHangedMan <chongwei.ma@outlook.com>
2023-01-13 20:26:00 +0000

    modified the code in challenge77

--- oop2/dice_contest.py
+++ oop2/dice_contest.py
@@ -12,10 +12,13 @@ def main():
     swapper = Cheat_Swapper()
     # the player known as the loaded_dice
     loaded_dice = Cheat_Loaded_Dice()
+    
+    regular_player = Player()
 
     # track scores for both players
     swapper_score = 0
     loaded_dice_score = 0
+    regular_player_score = 0
 
     # how many games we want to run
     number_of_games = 100000
@@ -27,6 +30,7 @@ def main():
     while game_number < number_of_games:
         swapper.roll()
         loaded_dice.roll()
+        regular_player.roll()
 
         swapper.cheat()
         loaded_dice.cheat()
@@ -37,17 +41,21 @@ def main():
 
         #print("Cheater 1 rolled" + str(swapper.get_dice()))
         #print("Cheater 2 rolled" + str(loaded_dice.get_dice()))
-        if sum(swapper.get_dice()) == sum(loaded_dice.get_dice()):
+        if sum(swapper.get_dice()) == sum(loaded_dice.get_dice()) == sum(regular_player.get_dice()):
             #print("Draw!")
             pass
-        elif sum(swapper.get_dice()) > sum(loaded_dice.get_dice()):
+        elif sum(swapper.get_dice()) > sum(loaded_dice.get_dice()) and sum(swapper.get_dice()) > sum(regular_player.get_dice()):
             #print("Dice swapper wins!")
             swapper_score+= 1
-        else:
+        elif sum(loaded_dice.get_dice()) > sum(swapper.get_dice()) and sum(loaded_dice.get_dice()) > sum(regular_player.get_dice()):
             #print("Loaded dice wins!")
             loaded_dice_score += 1
+        elif sum(regular_player.get_dice()) > sum(loaded_dice.get_dice()) and sum(regular_player.get_dice()) > sum(swapper.get_dice()):
+            regular_player_score += 1
+                      
         game_number += 1
 
+    
     # the game has ended
     print("Simulation complete")
     print("-------------------")
@@ -55,6 +63,7 @@ def main():
     print("------------")
     print(f"Swapper won: {swapper_score}")
     print(f"Loaded dice won: {loaded_dice_score}")
+    print(f"regular player won: {regular_player_score}")
 
     # determine the winner
     if swapper_score == loaded_dice_score:
